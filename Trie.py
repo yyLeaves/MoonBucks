b2708537b06ed7f0738b11611a3c5bc482480e81
yyLeaves <luckyyeyang@gmail.com>
2022-06-03 16:35:48 +0800

    add some finalization

--- Trie.py
+++ Trie.py
@@ -167,7 +167,23 @@ class trie_utils:
         return (int(sentiment[1]) / int(sentiment[-1])) / int(sentiment[0]) * 100
 
 
-from articles import cn
+from articles import articles
 
 if __name__ == '__main__':
+    trie = trie_utils().pickle_load_trie()
+
+    for country in articles.keys():
+        article_arr = trie_utils().preprocess(articles[country])
+
+        # If want other stats about words, run the below,
+        # you can fetch the stats in the dict
+        # res = trie.generate_sentiment_search_report(article_arr)
+        # sentiment = res['sentiment']
+
+        # If only want score, this will be more efficiency
+        sentiment = trie.sentiment_search(article_arr)
+        score = trie_utils().get_score(sentiment)
+        print(
+            f"{country} has {sentiment[0]} neutral words, {sentiment[-1]} negative words, {sentiment[1]} positive words, {sentiment[11]} stop words")
+        print(f"{country} has a score of {score}")
     pass
