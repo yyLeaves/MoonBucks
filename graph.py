fd7bfe5412dca981d09cb0d3c12dece1bdec4fc9
Koutkotsukan <ayersneo@gmail.com>
2022-06-19 00:31:36 +0800

    improve graph.py

--- graph.py
+++ graph.py
@@ -3,66 +3,51 @@ import plotly.graph_objs as pg
 import numpy as np
 
 
-class Graph:
-    def __init__(self):
-        self.country_name = []
-        self.positive_word = []
-        self.negative_word = []
-        self.neutral_word = []
-        self.stopwords_word = []
-        self.score = []
+def graph(country_detail):
+    print(country_detail)
+    country_detail = np.transpose(country_detail)
+    print(country_detail)
+    positive = pg.Bar(
+        x=country_detail[0],
+        y=np.array(country_detail[3]).astype(int),
+        name="Positive Words"
+    )
+    negative = pg.Bar(
+        x=country_detail[0],
+        y=np.array(country_detail[2]).astype(int),
+        name="Negative Words"
+    )
+    neutral = pg.Bar(
+        x=country_detail[0],
+        y=np.array(country_detail[1]).astype(int),
+        name="Neutral Words"
+    )
+    stopwords = pg.Bar(
+        x=country_detail[0],
+        y=np.array(country_detail[4]).astype(int),
+        name="Stopwords Words"
+    )
+    trace = [positive, neutral, negative, stopwords]
+    layout = pg.Layout(
+        title="Words Sentiment Analysis"
+    )
+    fig = pg.Figure(data=trace, layout=layout)
+    plotly.offline.plot(fig, filename='Bar.html')
 
-    def data_converter(self, country_detail):
-        self.country_name.append(country_detail[0])
-        self.neutral_word.append(country_detail[1])
-        self.negative_word.append(country_detail[2])
-        self.positive_word.append(country_detail[3])
-        self.stopwords_word.append(country_detail[4])
-        self.score.append(country_detail[5])
+    arr1 = np.array(country_detail[0])
+    arr2 = np.array(country_detail[5])
+    arrIndex = np.array(arr2).argsort()
+    arr1 = arr1[arrIndex]
+    arr2 = arr2[arrIndex]
 
-    def graph_maker(self):
-        positive = pg.Bar(
-            x=self.country_name,
-            y=self.positive_word,
-            name="Positive Words"
-        )
-        negative = pg.Bar(
-            x=self.country_name,
-            y=self.negative_word,
-            name="Negative Words"
-        )
-        neutral = pg.Bar(
-            x=self.country_name,
-            y=self.neutral_word,
-            name="Neutral Words"
-        )
-        stopwords = pg.Bar(
-            x=self.country_name,
-            y=self.stopwords_word,
-            name="Stopwords Words"
-        )
+    score_rank = pg.Scatter(
+        x=arr1,
+        y=arr2,
+        name="Score"
+    )
+    layout = pg.Layout(
+        title="Score Rank"
+    )
+    fig = pg.Figure(data=score_rank, layout=layout)
+    plotly.offline.plot(fig, filename="Rank.html")
 
-        trace = [positive, neutral, negative, stopwords]
-        layout = pg.Layout(
-            title="Words Sentiment Analysis"
-        )
-        fig = pg.Figure(data=trace, layout=layout)
-        plotly.offline.plot(fig, filename='Bar.html')
-
-    def score_ranker(self):
-        arr1 = np.array(self.country_name)
-        arr2 = np.array(self.score)
-        arrIndex = np.array(arr2).argsort()
-        arr1 = arr1[arrIndex]
-        arr2 = arr2[arrIndex]
-
-        score_rank = pg.Scatter(
-            x=arr1,
-            y=arr2,
-            name="Score"
-        )
-        layout = pg.Layout(
-            title="Score Rank"
-        )
-        fig = pg.Figure(data=score_rank, layout=layout)
-        plotly.offline.plot(fig, filename="Rank.html")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Trie import *
-from graph import Graph
 from articles import articles
 from moon_utils import pick_country
+from graph import graph
 
 if __name__ == '__main__':
 
@@ -13,11 +13,8 @@ if __name__ == '__main__':
     trie = trie_utils().pickle_load_trie()
 
     data_list = []
-
     # sentiment analysis by words
     for country in articles.keys():
-        data_list = []
-
         article_arr = trie_utils().preprocess(articles[country])
 
         # If want other stats about words, run the below,
@@ -30,10 +27,7 @@ if __name__ == '__main__':
         score = trie_utils().get_score(sentiment)
 
         data_list.append([country, sentiment[0], sentiment[-1], sentiment[1], sentiment[11], score])
-
         print(
             f"{country} has {sentiment[0]} neutral words, {sentiment[-1]} negative words, {sentiment[1]} positive words, {sentiment[11]} stop words")
         print(f"{country} has a score of {score}")
-    # graph = Graph()
-    # graph.graph_maker()
-    # graph.score_ranker()
+    graph(data_list)
