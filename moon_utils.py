853b3f221fb5f253d9d4efa4099353f221837039
yyLeaves <luckyyeyang@gmail.com>
2022-06-23 00:34:31 +0800

    add random sample method

--- moon_utils.py
+++ moon_utils.py
@@ -10,6 +10,22 @@ def pick_country(n):
     return topn
 
 
+def random_sample(country, csv_path, n):
+    df = pd.read_csv(csv_path)
+    df = df[df['state'] == country]
+    df = df[['longitude', 'latitude']]
+    stores = df.sample(n)
+    stores_dict = stores.to_dict()
+    lat = stores_dict['latitude']
+    lon = stores_dict['longitude']
+    x = [lat[e] for e in lat]
+    y = [lon[e] for e in lon]
+    return [[x[i], y[i]] for i in range(n)]
+
+
+stores = random_sample('US', csv_path, n)
+# stores
+
 if __name__ == '__main__':
     # US, CA, JP, CN, GB
     top5 = pick_country(5)
