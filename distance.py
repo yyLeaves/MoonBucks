8f908b48c8216fee13665b538769fbccb6140226
Neo <49346779+Koukotsukan@users.noreply.github.com>
2022-06-23 14:08:00 +0800

    fix a mistake

--- distance.py
+++ distance.py
@@ -1,52 +1,41 @@
-import plotly.graph_objs as pg
-import numpy as np
+from moon_utils import random_sample, pick_country
+from cfg import csv_path
+import math
+from geopy.distance import geodesic
 
 
-def bar_graph(country_detail):
-    country_detail = np.transpose(country_detail)
-    positive = pg.Bar(
-        x=country_detail[0],
-        y=np.array(country_detail[3]).astype(int),
-        name="Positive Words"
-    )
-    negative = pg.Bar(
-        x=country_detail[0],
-        y=np.array(country_detail[2]).astype(int),
-        name="Negative Words"
-    )
-    neutral = pg.Bar(
-        x=country_detail[0],
-        y=np.array(country_detail[1]).astype(int),
-        name="Neutral Words"
-    )
-    stopwords = pg.Bar(
-        x=country_detail[0],
-        y=np.array(country_detail[4]).astype(int),
-        name="Stopwords Words"
-    )
-    trace = [positive, neutral, negative, stopwords]
-    layout = pg.Layout(
-        title="Words Sentiment Analysis"
-    )
-    fig = pg.Figure(data=trace, layout=layout)
-    fig.write_html("Bar.html")
+def find_shortest(country, n):
+    cities_location = random_sample(country, csv_path, n)
+    print(cities_location)
+    distance_min = 0
+    distance_min_city = cities_location[0]
+    for h in range(1, n):
+        distance_min = distance_min + geodesic(cities_location[0], cities_location[h]).km
+    for i in cities_location:
+        cities_rest = cities_location.copy()
+        cities_rest.remove(i)
+        distance = 0
+        for e in range(n - 1):
+            distance = distance + geodesic(i, cities_rest[e]).km
+        print("Sum of distance for ", i, "is: ", distance, 'km')
+        if distance < distance_min:
+            distance_min = distance
+            distance_min_city = i
+    return [distance_min_city, distance_min]
+
+# def get_distance(p1, p2):
+#     x = p1[0] - p2[0]
+#     y = p1[1] - p2[1]
+#     return math.sqrt(x ** 2 + y ** 2)
 
 
-def score_graph(country_detail):
-    country_detail = np.transpose(country_detail)
-    arr1 = np.array(country_detail[0])
-    arr2 = np.array(country_detail[5]).astype(float)
-    arrIndex = np.array(arr2).argsort()
-    arr1 = arr1[arrIndex]
-    arr2 = arr2[arrIndex]
-
-    score_rank = pg.Scatter(
-        x=arr1,
-        y=arr2,
-        name="Score"
-    )
-    layout = pg.Layout(
-        title="Score Rank"
-    )
-    fig = pg.Figure(data=score_rank, layout=layout)
-    fig.write_html("Rank.html")
+if __name__ == '__main__':
+    m = 5
+    n = 6
+    countries_name = pick_country(m)
+    for i in countries_name:
+        print("-"*100, "\nCOUNTRY: ", str(i))
+        fs = find_shortest(str(i), n)
+        print("-" * 50, "\nThe local distribution center for ", str(i)," should be set at ", fs[0], ", which has a sum of distance: ",
+              fs[1], "km")
+    print("-"*100)
